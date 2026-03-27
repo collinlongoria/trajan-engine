106e114491e332a74a494d12d2a14d1c69a189c3
Collin Longoria <collin.longoria@digipen.edu>
2026-03-27 10:53:35 -0700

    initial commit with submodules

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+* File: main.cpp
+* Project: trajan-engine
+* Author: Collin Longoria
+* Created on: 3/27/2026
+*
+* Copyright (c) 2025 Collin Longoria
+*
+* This software is released under the MIT License.
+* https://opensource.org/licenses/MIT
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+}
